e5bcb9a0a8d4e893f645d5bb9d07bc1b5a2da565
Ahmedawad10 <80226442+Ahmedawad10@users.noreply.github.com>
2021-03-30 23:54:08 +0200

    Add files via upload

--- BOYKA.lua
+++ BOYKA.lua
@@ -2583,8 +2583,8 @@ local Text = [[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = 'مـطـوريـن الـسـورس',url="t.me/USER_ALFA"}},
-{{text = 'قـنـاة الـسـورس' url="t.me/so_alfaa"}},
-{{text = 'تـواصـل الـسـورس', url="t.me/TW_AHMEED_BOT"}},
+{{text = 'قـنـاة الـسـورس',url="t.me/so_alfaa"}},
+{{text = 'تـواصـل الـسـورس',url="t.me/TW_AHMEED_BOT"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
